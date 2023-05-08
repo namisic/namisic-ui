aee02d061b735aa54419c97c51b9f20e752a7128
Oscar David Díaz Fortaleché <lechediaz@gmail.com>
2023-05-07 23:14:43 -0500

    Issue 5: Allow to create a new Resident via modal

---
+++ src/components/residents/create-resident-modal.tsx
@@ -0,0 +1,67 @@
+import useResidentsApi from '@/hooks/use-residents-api';
+import { CreateOrUpdateResidentModel } from '@/types/resident-types';
+import { Form, notification } from 'antd';
+import React, { useEffect, useState } from 'react';
+import ResidentForm from './resident-form';
+import dynamic from 'next/dynamic';
+
+// Loaded with SSR disabled because portals fail with rehydration.
+// See https://nextjs.org/docs/pages/building-your-application/optimizing/lazy-loading#with-no-ssr
+const DynamicModal = dynamic(() => import('antd/es/modal'), {
+  ssr: false,
+});
+
+export interface CreateResidentModalProps {
+  openModal: boolean;
+  onClose?: () => void;
+}
+
+export const CreateResidentModal: React.FC<CreateResidentModalProps> = ({
+  openModal,
+  onClose,
+}) => {
+  const residentsApi = useResidentsApi();
+  const [formInstance] = Form.useForm();
+  const [isOpen, setIsOpen] = useState(false);
+  const onSaveClick = async (resident: CreateOrUpdateResidentModel) => {
+    await residentsApi.create(resident);
+    notification.success({
+      description: `El residente '${resident.name}' fue creado.`,
+      message: 'Operación realizada correctamente',
+    });
+    close();
+  };
+  const close = () => {
+    setIsOpen(false);
+    if (typeof onClose === 'function') {
+      onClose();
+    }
+  };
+  const onOk = () => {
+    formInstance.submit();
+  };
+
+  useEffect(() => {
+    setIsOpen(openModal);
+  }, [openModal]);
+
+  return (
+    <DynamicModal
+      title="Crear nuevo residente"
+      open={isOpen}
+      cancelText="Cancelar"
+      okText="Guardar"
+      afterClose={close}
+      onOk={onOk}
+      onCancel={close}
+    >
+      <ResidentForm
+        formInstance={formInstance}
+        hideSaveButon
+        onSaveClick={onSaveClick}
+      />
+    </DynamicModal>
+  );
+};
+
+export default CreateResidentModal;
--- src/pages/residents/[id].tsx
+++ src/pages/residents/[id].tsx
@@ -1,15 +1,19 @@
-import ResidentForm from "@/components/residents/resident-form";
-import useResidentsApi from "@/hooks/use-residents-api";
-import { ResidentModel } from "@/types/resident-types";
-import { Tabs, TabsProps, notification } from "antd";
-import { useRouter } from "next/router";
-import React, { Component, useCallback, useEffect, useState } from "react";
+import ResidentForm from '@/components/residents/resident-form';
+import useResidentsApi from '@/hooks/use-residents-api';
+import {
+  CreateOrUpdateResidentModel,
+  ResidentModel,
+} from '@/types/resident-types';
+import { Tabs, TabsProps, notification, Form } from 'antd';
+import { useRouter } from 'next/router';
+import React, { useCallback, useEffect, useState } from 'react';
 
 export function ResidentDetails() {
   const router = useRouter();
   const { id } = router.query;
   const residentsApi = useResidentsApi();
   const [resident, setResident] = useState<ResidentModel | undefined>();
+  const [formInstance] = Form.useForm();
 
   const getResident = useCallback(async () => {
     const resident = await residentsApi.getById(id as string);
@@ -24,31 +28,33 @@ export function ResidentDetails() {
     console.log(key);
   };
 
-  let items: TabsProps["items"] = [];
+  let items: TabsProps['items'] = [];
 
   if (resident !== undefined) {
-    const onSaveClick = async (resident: ResidentModel): Promise<void> => {
+    const onSaveClick = async (
+      resident: CreateOrUpdateResidentModel
+    ): Promise<void> => {
       await residentsApi.update(resident);
       notification.success({
         description: `El residente '${resident.name}' fue actualizado.`,
-        message: "Operación realizada correctamente",
+        message: 'Operación realizada correctamente',
       });
     };
 
     items = [
       {
-        key: "1",
+        key: '1',
         label: `Información`,
         children: (
           <ResidentForm
+            formInstance={formInstance}
             resident={resident}
-            action="Guardar"
             onSaveClick={onSaveClick}
           />
         ),
       },
       {
-        key: "2",
+        key: '2',
         label: `Vehículos`,
       },
     ];
