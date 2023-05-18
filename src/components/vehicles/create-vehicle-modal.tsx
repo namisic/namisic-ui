6ffee602b61aa2766e49c64090c1431aa9b9fe04
Oscar David Díaz Fortaleché <lechediaz@gmail.com>
2023-05-17 23:59:55 -0500

    Issue #14: Allow to create a vehicle

---
+++ src/components/vehicles/create-vehicle-modal.tsx
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from 'react';
+import dynamic from 'next/dynamic';
+import useVehiclesApi from '@/hooks/use-vehicles-api';
+import { Form, notification } from 'antd';
+import { CreateOrUpdateVehicleModel } from '@/types/vehicle-types';
+import VehicleForm from './vehicle-form';
+
+// Loaded with SSR disabled because portals fail with rehydration.
+// See https://nextjs.org/docs/pages/building-your-application/optimizing/lazy-loading#with-no-ssr
+const DynamicModal = dynamic(() => import('antd/es/modal'), {
+  ssr: false,
+});
+
+export interface CreateVehicleModalProps {
+  openModal: boolean;
+  residentId?: string;
+  onClose?: () => void;
+}
+
+const CreateVehicleModal: React.FC<CreateVehicleModalProps> = ({
+  openModal,
+  residentId,
+  onClose,
+}) => {
+  const vehiclesApi = useVehiclesApi();
+  const [formInstance] = Form.useForm();
+  const [isOpen, setIsOpen] = useState(false);
+  const onSaveClick = async (vehicle: CreateOrUpdateVehicleModel) => {
+    await vehiclesApi.create(vehicle);
+    notification.success({
+      description: `El vehículo con placa '${vehicle.plateNumber}' fue creado.`,
+      message: 'Operación realizada correctamente',
+    });
+    close();
+  };
+  const close = () => {
+    formInstance.resetFields();
+    setIsOpen(false);
+    if (typeof onClose === 'function') {
+      onClose();
+    }
+  };
+  const onOk = () => {
+    formInstance.submit();
+  };
+
+  useEffect(() => {
+    setIsOpen(openModal);
+
+    if (openModal) {
+      formInstance.setFieldValue('residentId', residentId);
+    }
+  }, [openModal]);
+
+  return (
+    <DynamicModal
+      title="Crear nuevo vehículo"
+      open={isOpen}
+      cancelText="Cancelar"
+      okText="Guardar"
+      afterClose={close}
+      onOk={onOk}
+      onCancel={close}
+    >
+      <VehicleForm
+        formInstance={formInstance}
+        hideSaveButon
+        onSaveClick={onSaveClick}
+      />
+    </DynamicModal>
+  );
+};
+
+export default CreateVehicleModal;
---
+++ src/components/vehicles/vehicle-form.tsx
@@ -0,0 +1,89 @@
+import { VehiclesTypesOptions } from '@/constants/vehicles-constants';
+import { CreateOrUpdateVehicleModel } from '@/types/vehicle-types';
+import { Button, Form, FormInstance, Input, Select } from 'antd';
+import React, { useEffect } from 'react';
+
+export interface VehicleFormProps {
+  formInstance: FormInstance;
+  vehicle?: CreateOrUpdateVehicleModel;
+  hideSaveButon?: true;
+  onSaveClick?: (vehicle: CreateOrUpdateVehicleModel) => Promise<void>;
+}
+
+const VehicleForm: React.FC<VehicleFormProps> = ({
+  formInstance,
+  hideSaveButon,
+  onSaveClick,
+  vehicle,
+}) => {
+  const onFinish = async (fieldsValue: any) => {
+    if (onSaveClick !== undefined) {
+      const vehicleToSave: CreateOrUpdateVehicleModel = {
+        ...fieldsValue,
+      };
+
+      if (vehicle != undefined) {
+        vehicleToSave.initialPlateNumber = vehicle.initialPlateNumber;
+      }
+
+      await onSaveClick(vehicleToSave);
+    }
+  };
+
+  useEffect(() => {
+    if (vehicle !== undefined) {
+      formInstance.setFieldsValue(vehicle);
+    }
+  }, [vehicle]);
+
+  return (
+    <Form
+      labelCol={{ span: 12 }}
+      wrapperCol={{ span: 12 }}
+      layout="horizontal"
+      onFinish={onFinish}
+      form={formInstance}
+      style={{ maxWidth: 600 }}
+    >
+      <Form.Item
+        hidden
+        name="residentId"
+        rules={[{ required: true, message: 'La Placa es obligatoria' }]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name="plateNumber"
+        label="Placa"
+        rules={[
+          { required: true, message: 'La Placa es obligatoria' },
+          { max: 8 },
+        ]}
+      >
+        <Input placeholder="Ejemplo: ABC-123" />
+      </Form.Item>
+      <Form.Item
+        name="type"
+        label="Tipo de Vehículo"
+        rules={[
+          {
+            required: true,
+            message: 'El Tipo de Vehículo es obligatorio',
+          },
+        ]}
+      >
+        <Select
+          options={VehiclesTypesOptions}
+          placeholder="Selecciona una opción"
+        />
+      </Form.Item>
+      {!hideSaveButon && (
+        <Button type="primary" htmlType="submit">
+          Guardar
+        </Button>
+      )}
+    </Form>
+  );
+};
+
+export default VehicleForm;
