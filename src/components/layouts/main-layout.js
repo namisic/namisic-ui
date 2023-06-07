cc54612905414b1823133aa63dedc4101dfb7f35
Oscar David Díaz Fortaleché <lechediaz@gmail.com>
2023-06-07 17:27:50 -0500

    Menu collapsing is allowed

--- src/components/layouts/main-layout.js
+++ src/components/layouts/main-layout.js
@@ -2,11 +2,11 @@ import { MainNavbar } from '../navbars/main-navbar';
 
 export const MainLayout = ({ children }) => {
   return (
-    <article className="md:flex md:flex-row md:h-screen">
+    <article className="w-full md:flex md:flex-row md:h-screen">
       <aside>
         <MainNavbar />
       </aside>
-      <main className="container pl-4 py-4 bg-white text-black">
+      <main className="md:container pl-4 py-4 bg-white text-black">
         {children}
       </main>
     </article>
--- src/components/navbars/main-navbar.tsx
+++ src/components/navbars/main-navbar.tsx
@@ -1,26 +1,39 @@
 import { Menu, MenuProps } from 'antd';
-import { ApartmentOutlined, AuditOutlined } from '@ant-design/icons';
+import {
+  ApartmentOutlined,
+  AuditOutlined,
+  MenuOutlined,
+} from '@ant-design/icons';
 import { useRouter } from 'next/router';
 import { getMenuItem } from './navbar-utils';
-
-const menuItems: MenuProps['items'] = [
-  getMenuItem('Administración', 'management', <ApartmentOutlined />, [
-    getMenuItem('Residentes', 'residents', null),
-  ]),
-
-  { type: 'divider' },
-
-  getMenuItem('Reportes', 'reports', <AuditOutlined />, [
-    getMenuItem('Entrada/Salida de Vehículos', 'io-vehicles', null),
-  ]),
-];
+import { useState } from 'react';
 
 export const MainNavbar = () => {
   const router = useRouter();
+  const [collapsed, setCollapsed] = useState(false);
+
+  const menuItems: MenuProps['items'] = [
+    getMenuItem(
+      collapsed ? 'Mostrar menú' : 'Cerrar menú',
+      'collapser',
+      <MenuOutlined />
+    ),
+    getMenuItem('Administración', 'management', <ApartmentOutlined />, [
+      getMenuItem('Residentes', 'residents', null),
+    ]),
+    { type: 'divider' },
+    getMenuItem('Reportes', 'reports', <AuditOutlined />, [
+      getMenuItem('Entrada/Salida de Vehículos', 'io-vehicles', null),
+    ]),
+  ];
 
   // Navigate to page.
   const onClick: MenuProps['onClick'] = (e) => {
-    router.push(`/${e.key}`);
+    if (e.key === 'collapser') {
+      setCollapsed((currentValue) => !currentValue);
+    } else {
+      router.push(`/${e.key}`);
+    }
   };
 
   return (
@@ -28,6 +41,7 @@ export const MainNavbar = () => {
       onClick={onClick}
       defaultSelectedKeys={['1']}
       defaultOpenKeys={['sub1']}
+      inlineCollapsed={collapsed}
       mode="inline"
       items={menuItems}
       theme="dark"
