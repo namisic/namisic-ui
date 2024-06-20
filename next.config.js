8476666c06a784d7e1c33bd237654b37a2adb76a
Oscar David Díaz Fortaleché <lechediaz@gmail.com>
2024-06-20 12:49:19 -0500

    Corrección de export en rc-util

--- next.config.js
+++ next.config.js
@@ -3,6 +3,18 @@ const nextConfig = {
   reactStrictMode: true,
   // Necesario según documentación para desplegar con Docker. https://github.com/vercel/next.js/tree/canary/examples/with-docker#in-existing-projects
   output: 'standalone',
+  // Solución mientras, por error con export en rc-util. https://github.com/ant-design/ant-design/issues/46053#issuecomment-1827836587
+  transpilePackages: [
+    'antd',
+    '@ant-design',
+    'rc-util',
+    'rc-pagination',
+    'rc-picker',
+    'rc-notification',
+    'rc-tooltip',
+    'rc-tree',
+    'rc-table',
+  ],
 };
 
 module.exports = nextConfig;
